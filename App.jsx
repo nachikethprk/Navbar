485328e46578b2e0080e93858c437e3860c07a3c
nachikethprk <126313865+nachikethprk@users.noreply.github.com>
2023-03-07 22:49:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Flipkart from "./Components/Flipkart";
+import Netflix from "./Netflix";
+import Nav  from "./Components/Nav";
+import Zee from "./Components/Zee";
+import "./global.css"
+
+
+const App=()=>{
+    return(
+      <>
+      <Demo data="HII" />
+    {/* <Netflix/>
+    <Nav/>
+    <Flipkart/>
+    <Zee/> */}
+      </>
+    )
+}
+export default App
---
+++ Demo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Demo=(props)=>{
+    return(
+        props.Demo
+    )
+}
